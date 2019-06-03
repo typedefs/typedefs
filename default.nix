8a7b09c3c7e353da737234950cdac6dbdbb65f04
Arian van Putten <arian.vanputten@gmail.com>
2019-06-03 12:27:49 +0200

    Use idrisPackages callPackage design pattern
    Package definitions now explicitly list their dependencies
    as arguments of a function, and they get injected by callPackage.
    
    Furthermore, we define the statebox idris packages as an overlay
    over the existing idris package infrastructure. This means statebox
    packages are available in the `idrisPackages` namespace.
    `idrisPackages.callPackage` will then also support statebox's own
    packages, or packages that they have imported from third parties.

--- default.nix
+++ default.nix
@@ -42,6 +42,22 @@
 }:
 
 let
+  config = {
+    packageOverrides = pkgs: {
+      idrisPackages = pkgs.idrisPackages.override {
+        # We override the 'idrisPackages' packaget set
+        # This means, that all these package definitions will 'see' eachother
+        #  e.g. if typedefs-parser depends on typedefs, callPackage will automatically inject it
+        # Add new idris packages to this list 
+        overrides = idrisPackagesNew: idrisPackagesOld: {
+          typedefs = idrisPackagesNew.callPackage ./typedefs.nix {};
+          typedefs-parser = idrisPackagesNew.callPackage ./typedefs-parser.nix {};
+          typedefs-parser-js = idrisPackagesNew.callPackage ./typedefs-parser-js.nix {};
+          typedefs-examples = idrisPackagesNew.callPackage ./typedefs-examples.nix {};
+        };
+      };
+    };
+  };
   syspkgs = import <nixpkgs> { };
   pinpkgs = syspkgs.fetchFromGitHub {
     owner = "NixOS";
@@ -51,19 +67,20 @@ let
     rev = "140a648e46f9a255849d938efd92930297258621";
     sha256 = "0az8ph3kf6pgz5xv2x1n4kiccyyq0p5h2rjqfwc5qh97lnqigmbf";
   };
-  usepkgs = if null == pkgs then
-             import pinpkgs {}
-            else
-              if 0 == pkgs then
-                import <nixpkgs> { }
-              else
-                import pkgs {};
+  usepkgs = (
+    if null == pkgs 
+    then import pinpkgs
+    else if 0 == pkgs 
+    then import <nixpkgs>
+    else import pkgs) { inherit config; };
   stdenv = usepkgs.stdenvAdapters.keepDebugInfo usepkgs.stdenv;
 
 in {
-  typedefs = usepkgs.callPackage ./typedefs.nix {};
-  typedefs-parser = usepkgs.callPackage ./typedefs-parser.nix {};
-  typedefs-parser-js = usepkgs.callPackage ./typedefs-parser-js.nix {};
-  typedefs-examples = usepkgs.callPackage ./typedefs-examples.nix {};
-
+  # This attribute set exposes what things will be built by a call to nix-build. See
+  # its as the "CI" entrypoint.
+  inherit (usepkgs.idrisPackages) 
+    typedefs 
+    typedefs-parser
+    typedefs-parser-js 
+    typedefs-examples;
 }
