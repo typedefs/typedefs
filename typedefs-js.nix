8c0cede91cd13dc6fd7704d19d36060e84763823
André Videla <andre.videla@gmail.com>
2019-08-21 10:52:14 +0200

    Fix nix build

--- typedefs-js.nix
+++ typedefs-js.nix
@@ -1,11 +1,11 @@
-{ build-idris-package, typedefs, tparsec, bytes, js }:
+{ build-idris-package, typedefs, tparsec, bytes, js, effects }:
 
 build-idris-package {
   name = "typedefs-js";
   version = "dev";
   src = ./.;
 
-  idrisDeps = [ tparsec bytes js ];
+  idrisDeps = [ tparsec bytes js effects ];
 
   meta = {
     description = "Programming language agnostic type construction language based on polynomials - parser - JavaScript";
